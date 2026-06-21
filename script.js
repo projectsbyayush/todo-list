fee891c4d169f6cbfde7ecafb9e6cf1c1b704661
projectsbyayush <projectsbyayush@outlook.com>
2026-06-21 15:59:51 +0530

    Completed Final Development

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function addtask() {
-
+    const tasks = document.querySelector("#task").value.trim();
+    if (tasks === "") return;
+    document.querySelector(".task-list").innerHTML +=
+        `<li>${tasks} <button onclick="this.parentElement.remove()">X</button></li>`;
+    document.querySelector("#task").value = "";
 }
